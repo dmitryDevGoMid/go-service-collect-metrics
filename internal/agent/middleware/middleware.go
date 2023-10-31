17a4eadc6cd4512f8af1afd5f45faffddb2f0a01
Александр Щербаков <aleksandrserbakov@MacBook-Pro-Aleksandr.local>
2023-10-31 21:37:54 +0300

    push iter8 fixed empty branch

--- internal/agent/middleware/middleware.go
+++ internal/agent/middleware/middleware.go
@@ -35,9 +35,9 @@ func (config *Client) OnBeforeRequest() {
 		c.SetHeader("Content-Type", "application/json")
 
 		// Проверяем конфигурацию по умолчанию идет сжатие GZIP
-		if config.cfg.Gzip.Enable {
-			//c.SetHeader("Content-Encoding", "gzip")
-		}
+		//if config.cfg.Gzip.Enable {
+		//c.SetHeader("Content-Encoding", "gzip")
+		//}
 
 		// Now you have access to Client and current Request object
 		// manipulate it as per your need
--- internal/server/config/config.go
+++ internal/server/config/config.go
@@ -73,9 +73,9 @@ func init() {
 	flag.BoolVar(&enableGzip, "gzip", false, "set gzip for agent and server")
 
 	//File
-	flag.BoolVar(&restoreFile, "r", true, "restore file")
-	flag.StringVar(&fileStoragePath, "f", "/tmp/metrics-db.json", "path file")
-	//flag.StringVar(&fileStoragePath, "f", "", "path file")
+	flag.BoolVar(&restoreFile, "r", false, "restore file")
+	//flag.StringVar(&fileStoragePath, "f", "/tmp/metrics-db.json", "path file")
+	flag.StringVar(&fileStoragePath, "f", "", "path file")
 	flag.IntVar(&storeIntervalFile, "i", 5, "store interval file")
 }
 
